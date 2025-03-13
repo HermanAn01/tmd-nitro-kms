72b37f18352ffa86f6bb7fe9213daf08fe3c33e9
Herman.An <herman.anecmarkets.com@mac.tail7fc4d7.ts.net>
2025-03-13 22:27:36 +1300

    asd

--- src/main/java/com/spotec/tmd/nitrokms/aws/AwsSecretsManagerService.java
+++ src/main/java/com/spotec/tmd/nitrokms/aws/AwsSecretsManagerService.java
@@ -1,14 +1,10 @@
 package com.spotec.tmd.nitrokms.aws;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
-
-import java.util.Map;
 
 @Service
 public class AwsSecretsManagerService {
@@ -35,24 +31,6 @@ public class AwsSecretsManagerService {
             // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
             throw e;
         }
-
-        String secret = getSecretValueResponse.secretString();
-
-        return secret;
-
-        /*// 你的 Secret 名称
-
-        GetSecretValueRequest request = GetSecretValueRequest.builder()
-                .secretId(secretName)
-                .build();
-        GetSecretValueResponse response = secretsManagerClient.getSecretValue(request);
-
-        try {
-            Map<String, String> secretMap = objectMapper.readValue(response.secretString(), Map.class);
-            return secretMap.get("privateKey");
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to get secret: " + e.getMessage(), e);
-        }*/
+        return getSecretValueResponse.secretString();
     }
 }
-
